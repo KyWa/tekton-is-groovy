67f6ac0fb88ca46eff4d7a5400c775d8897d0e0e
Kyle Walker <kwalker@Kyles-MBP.kywa.io>
2022-12-05 14:55:59 -0600

    updating installer script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Tekton
-kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/previous/v0.35.1/release.yaml
+kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
 # Install the Dashboard
 kubectl apply -f https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
